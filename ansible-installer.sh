f5ea1f9e796149325d1383455319493a420038fc
Paapa Abdullah Morgan <paapaabdullahm@gmail.com>
2018-01-19 07:44:48 +0000

    add ansible installer

---
+++ ansible-installer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install ansible
+if ! grep -q "ansible/ansible" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    echo "Adding Ansible PPA..."
+    sudo apt-add-repository -y ppa:ansible/ansible
+fi
+
+if ! hash ansible >/dev/null 2>&1; then
+    echo "Installing Ansible..."
+    sudo apt update
+    sudo apt install -y software-properties-common ansible git python-apt
+else
+    echo "Ansible already installed"
+fi
+
+# Show run process
+echo "\nCustomize the playbook desktop.yml"
+echo "to suit your needs, then run ansible with:"
+echo "  ansible-playbook desktop.yml --ask-become-pass \n"
