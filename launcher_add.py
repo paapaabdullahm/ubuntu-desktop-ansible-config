9e7ad74561b7b0a522cda90b27933389bb55ff6e
Paapa Abdullah Morgan <paapaabdullahm@gmail.com>
2018-01-20 19:19:04 +0000

    add script that append application to launcher

---
+++ launcher_add.py
@@ -0,0 +1,28 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gio, Gtk
+import dbus
+import sys
+
+def gsettings_get(schema, path, key):
+    if path is None:
+        gsettings = Gio.Settings.new(schema)
+    else:
+        gsettings = Gio.Settings.new_with_path(schema, path)
+    return gsettings.get_value(key)
+
+def gsettings_set(schema, path, key, value):
+    if path is None:
+        gsettings = Gio.Settings.new(schema)
+    else:
+        gsettings = Gio.Settings.new_with_path(schema, path)
+    return gsettings.set_strv(key, value)
+
+current_list = list(gsettings_get('com.canonical.Unity.Launcher', None, 'favorites'))
+
+if sys.argv[2]:
+   current_list.insert(int(sys.argv[2]), "application://" + sys.argv[1])
+else:
+   current_list.append("application://" + sys.argv[1])
+
+gsettings_set('com.canonical.Unity.Launcher', None, 'favorites', current_list)
