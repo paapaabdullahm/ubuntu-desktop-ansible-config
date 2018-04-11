9dfdcc92bed37f7e77e43d467baba23205545956
pam79 <paapaabdullahm@gmail.com>
2018-04-11 18:38:06 +0000

    refactor script for automation with ansible

--- apt_entries.py
+++ apt_entries.py
@@ -7,6 +7,7 @@ and `/etc/sources.list.d/*.list'.
 
 from __future__ import print_function
 from collections import defaultdict
+import aptsources.sourceslist
 import itertools
 import sys
 
@@ -17,46 +18,18 @@ def _get_python_packagename(basename):
     return 'python{0:s}-{1:s}'.format(version_part, basename)
 
 
-try:
-    import aptsources.sourceslist
-except ImportError as ex:
-    print(
-        "Error: {0!s}.\n\n"
-        "Do you have the '{1:s}' package installed?\n"
-        "You can do so with 'sudo apt-get install {1:s}'."
-        .format(ex, _get_python_packagename('apt')),
-        file=sys.stderr)
-    sys.exit(127)
-
-
 def get_duplicates(sourceslist):
     sentry_map = defaultdict(list)
     for se in sourceslist.list:
         if not se.invalid and not se.disabled:
             for c in (se.comps or (None,)):
                 sentry_map[(se.type, se.uri, se.dist, c)].append(se)
-
     return filter(lambda dupe_set: len(dupe_set) > 1, sentry_map.values())
-
-
-# def _argparse(args):
-#     import argparse
-#     parser = argparse.ArgumentParser(**dict(zip(
-#        ('description', 'epilog'), map(str.strip, __doc__.rsplit('\n\n', 1)))))
-#     parser.add_argument('-y', '--yes',
-#                        dest='apply_changes', action='store_const', const=True,
-#                        help='Apply all changes without question.')
-#    parser.add_argument('-n', '--no-act', '--dry-run',
-#                       dest='apply_changes', action='store_const', const=False,
-#                       help='Never apply changes; '
-#                             'only print what would be done.')
-#    return parser.parse_args(args)
 
 
 def _main():
     sourceslist = aptsources.sourceslist.SourcesList(False)
     duplicates = tuple(get_duplicates(sourceslist))
-
     if duplicates:
         for dupe_set in duplicates:
             orig = dupe_set.pop(0)
@@ -69,15 +42,11 @@ def _main():
                         orig.file, orig, dupe.file, dupe),
                     end='\n\n')
                 dupe.disabled = True
-
         print('\n{0} source entries were disabled:'.format(len(duplicates)),
               *itertools.chain(*duplicates), sep='\n  ', end='\n\n')
-
         sourceslist.save()
-
     else:
         print('No duplicate entries were found.')
-
     return 0
 
 
